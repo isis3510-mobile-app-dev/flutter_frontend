41c78ba508cfaf8820ce345c8d4d28d33ef62fff
YairAndrade1 <kidyarson@gmail.com>
2026-04-24 10:59:12 -0500

    Use real analytics feature ids for attachment uploads

--- lib/core/telemetry/telemetry_ids.dart
+++ lib/core/telemetry/telemetry_ids.dart
@@ -4,5 +4,5 @@ const String featureNfcReadId = '69bc1bf92fb48df4a28aaf07';
 const String featureAddPetId = '69bc446a2fb48df4a28aaf1e';
 const String featureEditPetId = '69bd781c3bc6bfe99e3d4b2a';
 const String featureRouteAddEventId = '69bc1d2f2fb48df4a28aaf0a';
-const String featureVaccineAttachmentUploadId = '6809f0a1c0de3510a0000001';
-const String featureEventAttachmentUploadId = '6809f0a1c0de3510a0000002';
+const String featureVaccineAttachmentUploadId = '69eb92ef4e5c181e934ea67f';
+const String featureEventAttachmentUploadId = '69eb93064e5c181e934ea680';
